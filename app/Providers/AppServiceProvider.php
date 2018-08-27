735e470e2fe65d15163d04c5c0bcdfda14358262
Imran Ijaz <imran.ijaz82@gmail.com>
2018-08-27 20:16:54 +0500

    SERVICES

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,8 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-		Schema::defaultStringLength(191);        //
-    }
+		Schema::defaultStringLength(191);
+	}
 
     /**
      * Register any application services.
